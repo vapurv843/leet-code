6c1b111376cf5acba070c578bf0b279f4fd40bfa
Apurv verma <vapurv843@gmail.com>
2021-12-07 23:11:58 +0530

    binary search

---
+++ Week2/all_indexes_where_i_equals_target.cpp
@@ -0,0 +1,18 @@
+//question link:- https://leetcode.com/problems/ all-indexes-where-value-appears-in-sorted-array/
+
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<int> ans;
+        for(int i = 0;i<nums.size();i++)
+        {
+            if(nums[i]==target)
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+        
+    }
+};
