073e2a0b3a4845a6a77c192d27a5c145c816b18d
Apurv verma <vapurv843@gmail.com>
2022-01-10 12:48:39 +0530

    gfg

---
+++ Amazon_interview_practice/maximum_money.cpp
@@ -0,0 +1,10 @@
+int maximizeMoney(int N , int K) {
+        int sum = 0;
+        for(int i = 0;i<N;i+=2)
+        {
+            sum+=K;
+            
+        }
+        return sum;
+        
+    }
